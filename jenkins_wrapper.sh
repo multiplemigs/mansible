f52b120db56d921d350bda9c7e9523b44b270a06
Miguel <martinezmi@gmail.com>
2015-08-02 12:04:44 -0400

    adding jenkins wrapper

---
+++ jenkins_wrapper.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#Jenkins will pass tagname, buildname and optionally source/owner information to be used as ec2 Tag Keys
+while getopts ":n:b:s:" o; do
+    case "${o}" in
+        n)
+            tagname=${OPTARG}
+            ;;
+        b)
+            buildname=${OPTARG}
+            ;;
+        s)
+            buildname=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+#Error if Jenkins does not pass tagname or Buildname
+if [ -z "${tagname}" ] || [ -z "${buildname}" ]; then
+  echo "ERROR: jenkins must provide a tagname and buildname using -n and -b. source must also be provided with -s source"
+  exit 1
+fi
+
+#Run playbook for node provision
+ansible-playbook -i /etc/ansible/hosts /etc/ansible/playbooks/provision_dev_conf_tomcatapp.yml --vault-password-file /home/jenkins/.vecsss --extra-vars "tagname=${tagname} buildname=${buildname} source=${soure}"
+
+#Error check
+if [ $? -eq 0 ]
+then
+  echo "playbook run OK..."
+  echo "refreshing inventory file"
+  #refresh ansible-inventory and show new node in inventory
+  /etc/ansible/hosts --refresh-cache|grep -A 10 "tag_Name_${tagname}"
+  if [ $? -eq 0 ]
+  then
+    echo "Successfully ran playbook!"
+    echo "inventory refresh complete!"
+    exit 0
+  else
+    echo "playbook run successful"
+    echo "could not refresh inventory file" >&2
+    exit 1
+  fi
+else
+  echo "Could not run playbook" >&2
+  exit 1
+fi
